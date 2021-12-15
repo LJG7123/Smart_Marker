aca3454688b18ea9610c421c3adef7ba19952a4e
1sungwon1 <ysw502@naver.com>
2021-12-15 23:11:26 +0900

    스플래쉬

---
+++ app/src/main/java/com/example/smartmarker/intro.java
@@ -0,0 +1,34 @@
+package com.example.smartmarker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.smartmarker.IntroThread;
+
+public class intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        IntroThread introThread = new IntroThread(handler);
+        introThread.start();
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                Intent intent = new Intent(com.example.smartmarker.intro.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    };
+
+}
